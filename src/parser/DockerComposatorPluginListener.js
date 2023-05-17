bdc81597e2142db959d91baa3775ea1bf2d60faf
Yassine-Chamkhi <yessinechamkhi@gmail.com>
2023-05-17 23:53:36 +0100

    add parser and listener

---
+++ src/parser/DockerComposatorPluginListener.js
@@ -0,0 +1,120 @@
+import { Component, ComponentAttribute } from 'leto-modelizer-plugin-core';
+
+/**
+ * Lidy listener for Docker Compsoe files.
+ */
+class DockerComposatorPluginListener {
+/**
+   * Default constructor.
+   *
+   * @param {FileInformation} fileInformation - File information.
+   * @param {ComponentDefinition[]} [definitions=[]] - All component definitions.
+   */
+constructor(fileInformation, definitions = []) {
+    /**
+     * File information.
+     *
+     * @type {FileInformation}
+     */
+    this.fileInformation = fileInformation;
+    /**
+     * Array of component definitions.
+     *
+     * @type {ComponentDefinition[]}
+     */
+    this.definitions = definitions;
+    /**
+     * Parsed components.
+     */
+    this.components = [];
+    /**
+     * Parsed subcomponent.
+     */
+    this.childComponentsByType = {};
+  }
+
+
+   /**
+   * Function called when attribute `root` is parsed.
+   * Create a component from the parsed root element.
+   *
+   * @param {MapNode} rootNode - The Lidy `root` node.
+   */
+   exit_root(rootNode) {
+    const type = rootNode.value.type.value;
+
+
+    const rootComponent = this.createComponentFromTree(rootNode, type);
+    rootComponent.path = this.fileInformation.path;
+    rootComponent.definition.childrenTypes.forEach((childType) => {
+      this.setParentComponent(rootComponent, this.childComponentsByType[childType]);
+    });
+  }
+
+
+  createComponentFromTree(node, type) {
+    const definition = this.definitions.find((definition) =>
+        definition.type === type
+    );
+    const id = node.value.metadata?.value.name?.value || node.value.name?.value
+      || this.pluginData.generateComponentId(definition);
+
+    delete node.value.metadata?.value.name; // we don't want to create an attribute for the name, because the component already has a name
+    delete node.value.name; // TODO: improve this
+
+    const component = new Component({
+      id,
+      definition,
+      attributes: this.createAttributesFromTreeNode(node, definition),
+    });
+    this.components.push(component);
+    return component;
+  }
+
+
+  createAttributesFromTreeNode(parentNode, parentDefinition) {
+    return Object.keys(parentNode.value).map(childKey => {
+      const childNode = parentNode.value[childKey];
+      const definition = parentDefinition?.definedAttributes.find(
+        ({ name }) => name === (parentNode.type !== 'list' ? childKey: null)
+      ); // note: elements inside a list don't have a name, because it has to match the definition
+      const attribute = new ComponentAttribute({
+        name: childKey,
+        type: this.lidyToLetoType(childNode.type),
+        definition,
+        value: (childNode.type === 'map' || childNode.type === 'list') ?
+          this.createAttributesFromTreeNode(childNode, definition) :
+          childNode.value,
+      });
+      return attribute;
+    });
+  }
+
+  setParentComponent(parentComponent, childComponents) {
+    childComponents?.forEach((childComponent) => {
+      childComponent.setReferenceAttribute(parentComponent);
+    });
+  }
+
+  lidyToLetoType(lidyType) {
+    switch (lidyType) {
+      case 'string':
+        return 'String';
+      case 'boolean':
+        return 'Boolean';
+      case 'int':
+      case 'float':
+        return 'Number';
+      case 'map':
+        return 'Object';
+      case 'list':
+        return 'Array';
+      default:
+        return null; // TODO: throw error
+    }
+  }
+
+}
+
+
+export default DockerComposatorPluginListener;
--- src/parser/DockerComposatorPluginParser.js
+++ src/parser/DockerComposatorPluginParser.js
@@ -1,31 +1,92 @@
 import { DefaultParser } from 'leto-modelizer-plugin-core';
-
+import {DockerComposatorPluginListener} from 'DockerComposatorPluginListener.js'
 /**
  * Template of plugin parser.
  */
 class DockerComposatorPluginParser extends DefaultParser {
   isParsable(fileInformation) {
-    /*
+    /* 
      * Implement this to indicate which fileInformation your provider can manage.
      *
      * You just have to return a Boolean to say if your parser can manage the file or not.
      *
      * By default, this function return false only on null/undefined fileInformation.
      */
-    return super.isParsable(fileInformation);
+    return /\.ya?ml$/.test(fileInformation.path);
   }
 
-  parse(inputs = []) {
-    /*
-     * Implement your own parse function here.
-     *
-     * You receive in `inputs` a list of content file.
-     *
-     * In our plugin managing the terraform files, we use antlr for parsing. You can find an example
-     * of the terraform parser in https://github.com/ditrit/iactor/blob/dev/src/parser/TerraformParser.js.
-     */
+  /**
+   * Convert the content of files into Components.
+   *
+   * @param {FileInput[]} [inputs=[]] - Data you want to parse.
+   * @param {string} [parentEventId=null] - Parent event id.
+   */
+  parse(inputs = [], parentEventId = null) {
     this.pluginData.components = [];
     this.pluginData.parseErrors = [];
+
+    inputs
+      .filter(({ content, path }) => {
+        if (content && content.trim() !== '') {
+          return true;
+        }
+        this.pluginData.emitEvent({
+          parent: parentEventId,
+          type: 'Parser',
+          action: 'read',
+          status: 'warning',
+          files: [path],
+          data: {
+            code: 'no_content',
+            global: false,
+          },
+        });
+        return false;
+      })
+      .forEach((input) => {
+        const id = this.pluginData.emitEvent({
+          parent: parentEventId,
+          type: 'Parser',
+          action: 'read',
+          status: 'running',
+          files: [input.path],
+          data: {
+            global: false,
+          },
+        });
+
+        const listener = new DockerComposatorPluginListener(input, this.pluginData.definitions.components);
+
+        const errors = [];
+        const warnings = [];
+        const imports = [];
+        const alreadyImported = [];
+        const root = [];
+
+        lidyParse({
+          src_data: input.content,
+          listener,
+          path: input.path,
+          prog: {
+            errors: errors,
+            warnings: warnings,
+            imports: imports,
+            alreadyImported: alreadyImported,
+            root: root,
+          },
+        });
+
+        console.log(errors);
+        console.log(warnings);
+        console.log(imports);
+        console.log(alreadyImported);
+        console.log(root);
+
+        this.pluginData.components.push(...listener.components);
+        this.pluginData.emitEvent({ id, status: 'success' });
+      });
+
+    console.log('P', this.pluginData.components);
   }
 }
 
