ea47605ecac6e298b201c74f427fe1ce6e42b298
Yassine-Chamkhi <yessinechamkhi@gmail.com>
2023-05-17 19:03:33 +0100

    add parser

---
+++ src/lidy/dcompose.js
@@ -0,0 +1,6 @@
+import { parse as parse_input } from 'lidy-js';
+let rules={"main":"root","root":"docker-compose","docker-compose":{"_map":{"version":"string","services":{"_mapOf":{"any":"service"}}}},"service":{"_map":{"image":"string"},"_mapFacultative":{"stdin_open":"bool","tty":"bool","environment":{"_listOf":"string"},"ports":{"_listOf":"string"}}}}
+export function parse(input) { 
+  input.rules = rules
+  return parse_input(input)
+}
