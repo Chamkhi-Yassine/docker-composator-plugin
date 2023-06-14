6b6b7e6d2f4dd3fa4991c6901e95c5406f887c90
Yassine-Chamkhi <yassine.chamkhi@nearteam.fr>
2023-06-14 15:04:30 +0100

    write tests for parser and listener

---
+++ tests/resources/empty-compose.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-restricted-imports */
+import { ComponentAttribute } from 'leto-modelizer-plugin-core';
+import DockerComposatorData from '../../src/models/DockerComposatorData';
+import DockerComposatorPluginComponent from '../../src/models/DockerComposatorPluginComponent';
+import DockerComposatorPluginMetadata from '../../src/metadata/DockerComposatorPluginMetadata';
+
+const pluginData = new DockerComposatorData();
+const metadata = new DockerComposatorPluginMetadata(pluginData);
+metadata.parse();
+
+const dockerComposeDef = pluginData.definitions.components
+  .find(({ type }) => type === 'Docker-Compose');
+
+const dockerCompose = new DockerComposatorPluginComponent({
+  id: 'empty-compose',
+  path: './empty-compose.yaml',
+  definition: dockerComposeDef,
+  attributes: [
+    new ComponentAttribute({
+      name: 'version',
+      type: 'String',
+      definition: dockerComposeDef.definedAttributes
+        .find(({ name }) => name === 'version'),
+      value: '3.9',
+    }),
+  ],
+});
+
+pluginData.components.push(dockerCompose);
+
+export default pluginData;
--- tests/unit/parser/DockerComposatorPluginParser.spec.js
+++ tests/unit/parser/DockerComposatorPluginParser.spec.js
@@ -47,16 +47,29 @@ describe('Test DockerComposatorPluginParser', () => {
     });
 
     describe('Test function: parse', () => {
-    // it('Should set empty components on no input files', () => {
-    //   const pluginData = new DefaultData();
-    //   const parser = new DockerComposatorPluginParser(pluginData);
-    //   parser.parse();
-    //
-    //   expect(pluginData.components).not.toBeNull();
-    //   expect(pluginData.components.length).toEqual(0);
-    // });
+      it('Should set empty components on no input files', () => {
+        const pluginData = new DockerComposatorData();
+        const parser = new DockerComposatorPluginParser(pluginData);
+        parser.parse();
 
-      it('Parse simple.yml should set valid component', () => {
+        expect(pluginData.components).not.toBeNull();
+        expect(pluginData.components.length).toEqual(0);
+      });
+
+      it('Should set empty components on null input files', () => {
+        const pluginData = new DockerComposatorData();
+        const parser = new DockerComposatorPluginParser(pluginData);
+        const file = new FileInput({
+          path: '',
+          content: null,
+        });
+        parser.parse([file]);
+
+        expect(pluginData.components).not.toBeNull();
+        expect(pluginData.components.length).toEqual(0);
+      });
+
+      it('Parse should set valid component', () => {
         const pluginData = new DockerComposatorData();
         const metadata = new DockerComposatorPluginMetadata(pluginData);
         metadata.parse();
